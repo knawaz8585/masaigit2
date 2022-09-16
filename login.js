e9c99f910e4bc493eb0ba7e9f442ea78bc1723ea
Pushkarsingh28 <bishtpushkar852@gmail>
2022-09-17 01:21:28 +0530

    login logic

--- login.js
+++ login.js
@@ -1,2 +1,11 @@
 reg_email="knawaz492@gmail.com"
 reg_pass="No1Master"
+
+enter_email="knawaz492@gmail.com";
+enter_pass="No1Master";
+
+if(reg_email===enter_email && reg_pass===enter_pass){
+         console.log("login successfull");
+}else{
+         console.log("login failed");
+}
